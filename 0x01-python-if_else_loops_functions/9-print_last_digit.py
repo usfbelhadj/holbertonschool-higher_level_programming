772d1cbaa1e195233f3e7b27b672886a693777e3
usfbelhadj <usf.belhadj@gmail.com>
2020-04-28 22:05:39 +0100

    fiix _9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     number = str(number)
-    last = number[-1]
-    print(last, end="")
-    return last
+    print(number[-1], end="")
+    return number[-1]
