c963c5ec84626c140523ce0103831554f6ac1976
usfbelhadj <usf.belhadj@gmail.com>
2020-04-29 04:00:58 +0100

    Test

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,9 +5,10 @@ int check_cycle(listint_t *list)
 {
 listint_t *s = list, *f = list;
 
-while (s && f->next)
+while (s && f && f->next)
 {
 s = s->next->next;
+f = f->next;
 if (s == f->next)
 {
 return (1);
@@ -15,3 +16,15 @@ return (1);
 }
 return (0);
 }
+/*
+    while (slow_p && fast_p && fast_p->next) { 
+        slow_p = slow_p->next; 
+        fast_p = fast_p->next->next; 
+        if (slow_p == fast_p) { 
+
+            return 1; 
+        } 
+    } 
+    return 0; 
+} 
+*/
