079f6f46f4851a786533324bd191f64ea333e5cb
usfbelhadj <usf.belhadj@gmail.com>
2020-04-29 15:35:59 +0100

    Remove_char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    l = len(str)
-    if n > l or n < 0:
+    lang = len(str)
+    if n > lang or n < 0:
         return str
     else:
         for c in str:
