efdc230bd138ae757f526c57cd127af1f084a8a8
usfbelhadj <usf.belhadj@gmail.com>
2020-04-30 06:15:58 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+long = len(sys.argv)
+if long - 1 == 0:
+    print("{} {}".format(long - 1, "arguments."))
+if long - 1 > 0:
+    print("{} {}".format(long - 1, "arguments:"))
+for a in range(long):
+    if a == 0:
+        continue
+    print("{}: {}".format(a, sys.argv[a]))
