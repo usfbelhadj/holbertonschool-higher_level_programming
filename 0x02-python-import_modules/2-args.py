48184955a791af8fccfc3a9d95df14ff3c02d323
usfbelhadj <usf.belhadj@gmail.com>
2020-04-30 16:55:01 +0100

    Add set

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-long = len(sys.argv)
-if long - 1 == 0:
-    print("{} {}".format(long - 1, "arguments."))
-if long - 1 > 0:
-    print("{} {}".format(long - 1, "arguments:"))
-for a in range(long):
-    if a == 0:
-        continue
-    print("{}: {}".format(a, sys.argv[a]))
+if __name__ == "__main__":
+    import sys
+    long = len(sys.argv)
+    if long - 1 == 0:
+        print("{} {}".format(long - 1, "arguments."))
+    if long - 1 > 0:
+        print("{} {}".format(long - 1, "arguments:"))
+    for a in range(long):
+        if a == 0:
+            continue
+        print("{}: {}".format(a, sys.argv[a]))
