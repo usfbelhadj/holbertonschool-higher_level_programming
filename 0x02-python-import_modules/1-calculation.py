54d39dacd9d81bad6f4e3e2f2645ebdee81c78d3
usfbelhadj <usf.belhadj@gmail.com>
2020-04-30 16:57:10 +0100

    Ass set

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, calculator_1.add(a, b)), end="\n")
-print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)), end="\n")
-print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)), end="\n")
-print("{} / {} = {}".format(a, b, calculator_1.div(a, b)), end="\n")
+if __name__ == "__main__":
+    import calculator_1
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)), end="\n")
+    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)), end="\n")
+    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)), end="\n")
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)), end="\n")
