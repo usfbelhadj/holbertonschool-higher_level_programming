06033e01235ae392a2f9edd1629d9282214d72c9
usfbelhadj <usf.belhadj@gmail.com>
2020-04-30 05:36:20 +0100

    Calc

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import calculator_1
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)), end="\n")
+print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)), end="\n")
+print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)), end="\n")
+print("{} / {} = {}".format(a, b, calculator_1.div(a, b)), end="\n")
