4f55298b4db827695dea9e7e6869ae679899b3ec
usfbelhadj <usf.belhadj@gmail.com>
2020-05-05 21:18:11 +0100

    muml_return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence[0] == ' ':
-        return None(len(sentence), None)
+        return (len(sentence), None)
     return (len(sentence), sentence[0])
