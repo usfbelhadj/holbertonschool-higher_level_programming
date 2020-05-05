0558c307bdffe19ec6fa4dbc1c6fea16b1f9c40b
usfbelhadj <usf.belhadj@gmail.com>
2020-05-05 22:13:16 +0100

    Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
-    j = 0
-    for i in my_list:
-        if j < i:
-            j = i
-    return j
+    my_list.sort()
+    max_integ = my_list[-1]
+    return (max_integ)
