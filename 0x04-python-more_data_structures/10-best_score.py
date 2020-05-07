3e6c74250b342e56593ad2f7b28422ba61e8798b
usfbelhadj <usf.belhadj@gmail.com>
2020-05-07 22:48:10 +0100

    best_scorw

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary or len(a_dictionary) == 0:
+    if not a_dictionary:
         return None
     else:
-        return max(a_dictionary)
+        key_list = list(a_dictionary.keys()) 
+        val_list = list(a_dictionary.values()) 
+        val = max(val_list)
+        for k in a_dictionary.values():
+            if val == k:
+                return(key_list[val_list.index(k)]) 
