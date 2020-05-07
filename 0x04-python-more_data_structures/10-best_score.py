2b2f0fdcb1ab2b44990000c96a1d5cea27abfc77
usfbelhadj <usf.belhadj@gmail.com>
2020-05-07 22:29:44 +0100

    Test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if not a_dictionary or len(a_dictionary) == 0:
         return None
     else:
         return max(a_dictionary)
