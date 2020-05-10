9c8bed10750566cab61d0a8524af1e0c07978161
usfbelhadj <usf.belhadj@gmail.com>
2020-05-10 19:11:11 +0100

    Update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return (list(map(lambda x: replace if my_list[search] == x else x, my_list)))
+    mat = my_list[:]
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            mat[i] = replace
+    return mat
