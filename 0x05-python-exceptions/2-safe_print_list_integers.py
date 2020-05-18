f62b3583aedbcc707fa8d72658b1b14e6f21e81e
usfbelhadj <usf.belhadj@gmail.com>
2020-05-18 22:24:27 +0100

    Safe_list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     j = 0
     while i < x:
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end='')
             j += 1
         except:
             pass
