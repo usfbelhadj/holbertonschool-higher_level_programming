97afbe8179dbeccb804471439cf4a8ed8805076d
usfbelhadj <usf.belhadj@gmail.com>
2020-05-19 20:10:55 +0100

    Privet

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module Square"""
+
+
+class Square:
+    """Defines Square module"""
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size: int
+            size = size of square
+        """
+        self.__size = size
