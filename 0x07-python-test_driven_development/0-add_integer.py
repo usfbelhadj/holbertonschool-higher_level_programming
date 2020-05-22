0fa227057b2bcab4545d82b56ac83ffa2301ecb2
usfbelhadj <usf.belhadj@gmail.com>
2020-05-22 01:15:38 +0100

    Add_integer with test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module add_integer"""
+
+
+def add_integer(a, b=98):
+    """
+        Parameters
+        ----------
+        a: int
+            a = First integer
+        b: int
+            b = Second integer
+        """
+    if type(a) not in (int, float):
+        raise TypeError('a must be an integer')
+    if type(b) not in (int, float):
+        raise TypeError('b must be an integer')
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
