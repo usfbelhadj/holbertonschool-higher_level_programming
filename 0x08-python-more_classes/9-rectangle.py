032334d807823cbeb6ce550f26388b1554e5b818
usfbelhadj <usf.belhadj@gmail.com>
2020-05-26 00:12:46 +0100

    Square

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
         width: int
             width of Rectangle
         """
-        self.height = height
         self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
