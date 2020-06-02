1c990e8602f86d872c433989a9fddf4ffbf52501
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 19:17:36 +0100

    Test_Fix_Doctest

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,3 +9,7 @@ class MyList(list):
         """Method That Print a sorted List
         """
         print(sorted(self))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
