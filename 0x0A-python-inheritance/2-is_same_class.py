24fbcf63635c6d55ad517f6e3044727e1caf817e
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 19:28:27 +0100

    Test_Fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+'''Mylist Class'''
+
+
 def is_same_class(obj, a_class):
-    """Return Same Class
+    """Return object if exactly an instance of the specified class
     """
     if type(obj) == a_class:
         return True
