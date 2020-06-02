e653996b12148e702f665ddf7c98946485be026b
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 19:34:26 +0100

    Fixing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """Return Not Same Class
     """
-    if type(obj) == a_class:
-        return False
-    else:
+    if type(obj) is not a_class:
         return True
+    else:
+        return False
