6ffd178cde7737813c1aa7c189fbe3c557dca1e2
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 20:49:41 +0100

    Try

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -7,6 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     '''Rectangle Class'''
+
     def __init__(self, width, height):
         '''Constructor'''
         self.integer_validator("width", width)
