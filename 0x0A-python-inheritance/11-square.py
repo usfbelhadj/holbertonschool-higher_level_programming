76367826631285a02d7804037a06b0f2a0f28f0a
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 21:36:17 +0100

    Try It

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Rectangle Class'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square Class'''
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''Returns area'''
+        return self.__size * self.__size
+
+    def __str__(self):
+        '''Print String'''
+        return '[Rectangle] ' + str(self.__size) + '/' + str(self.__size)
