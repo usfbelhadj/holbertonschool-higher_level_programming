aca648265c476f8e277c7d51f21921a80ac05a9b
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 19:37:54 +0100

    Testing X

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,4 @@
 def inherits_from(obj, a_class):
     """Return Not Same Class
     """
-    if type(obj) is not a_class:
-        return True
-    else:
-        return False
+    return isinstance(obj, a_class) and type(obj) is not a_class
