5265a0659259c3d7d16125f756b95458cc57b44a
usfbelhadj <usf.belhadj@gmail.com>
2020-06-02 19:06:49 +0100

    Test

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+'''lookup method'''
+
+
 def lookup(obj):
+    '''attributes and methods of an object
+    Args:
+        obj (object): list object
+    Returns:
+        list: list
+    '''
     return dir(obj)
