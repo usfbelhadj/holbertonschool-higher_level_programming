1bc87110d680f633f1a59a18f7dd08a997147efa
usfbelhadj <usf.belhadj@gmail.com>
2020-06-03 18:32:05 +0100

    Try

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-'''read_file Function'''
+'''number_of_lines Function'''
 
 
 def number_of_lines(filename=""):
+    '''number_of_lines: Sum of lines in the file
+       filename: Name of the file
+    '''
     with open(filename, 'r', encoding='utf-8') as f:
         line_num = 0
         lines = f.readlines()
-        for line in lines:
+        for line in range(len(lines)):
             line_num += 1
-        return line_num
+        return(line_num)
