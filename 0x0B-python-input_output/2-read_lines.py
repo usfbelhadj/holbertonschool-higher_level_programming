f39e90ebc4f9f62651d7e9c2baebf9abcd495c99
usfbelhadj <usf.belhadj@gmail.com>
2020-06-03 19:49:10 +0100

    Test

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''read_lines Function'''
+
+
+def read_lines(filename="", nb_lines=0):
+    '''read_lines: Reading lines
+       filename: Name of the file
+       nb_lines: Number Of The Line
+    '''
+    with open(filename, 'r', encoding='utf-8') as f:
+        if nb_lines <= 0:
+            print(f.read(), end='')
+        else:
+            for line in range(nb_lines):
+                print(f.readline())
