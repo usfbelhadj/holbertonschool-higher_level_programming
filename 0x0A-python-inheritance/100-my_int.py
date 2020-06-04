0009a9c9fbf787ed1b73d031a8e9cbc317714ad1
usfbelhadj <usf.belhadj@gmail.com>
2020-06-04 22:26:31 +0100

    Test

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''MyInt Class'''
+
+
+class MyInt(int):
+    def __init__(self, num):
+        self.num = num
+
+    def controle(self):
+        if self.num == num:
+            return False
+        else:
+            return True
