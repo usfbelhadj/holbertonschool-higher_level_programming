679002b45b9c4ab45e5666f73e50f32f7032e580
usfbelhadj <usf.belhadj@gmail.com>
2020-06-07 15:00:17 +0100

    Add Validation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,6 +18,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.validation("width", value, False)
         self.__width = value
 
     @property
@@ -26,6 +27,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validation('height', value, False)
         self.__height = value
 
     @property
@@ -34,6 +36,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validation('x', value)
         self.__x = value
 
     @property
@@ -42,4 +45,13 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validation('y', value)
         self.__y = value
+
+    def validation(self, name, value, bool=True):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if bool and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not bool and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
