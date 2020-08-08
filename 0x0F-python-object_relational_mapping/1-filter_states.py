ffd095199bd4ef6a658e09510b6ddaad2bffe31a
usfbelhadj <usf.belhadj@gmail.com>
2020-08-08 19:08:54 +0100

    Filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
             )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states where name like 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states where name like 'N%'")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
