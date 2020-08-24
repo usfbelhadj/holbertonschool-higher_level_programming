1b13ed0298107c35f7882a63adebc26c9e10646f
usfbelhadj <usf.belhadj@gmail.com>
2020-08-24 21:57:17 +0100

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Python script that fetches https://intranet.hbtn.io/status'''
+import requests
+if __name__ == "__main__":
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req)))
+    print("\t- content: {}".format(req))
