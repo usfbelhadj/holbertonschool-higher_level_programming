e6bf54fbc18d1a459119a843117300341ad14dbd
usfbelhadj <usf.belhadj@gmail.com>
2020-08-24 21:38:39 +0100

    post_email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Write a Python script that takes in a URL and an email, sends a POST request'''
+'''URL and an email sends a POST request'''
 from urllib import request, parse
 import sys
 if __name__ == "__main__":
