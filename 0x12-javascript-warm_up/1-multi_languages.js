b930784f29b1881fef81197a7a50b86c712f00b1
usfbelhadj <usf.belhadj@gmail.com>
2020-09-15 14:18:12 +0100

    Multi_lang

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+First constant, first print
+*/
+'use strict';
+
+const c = 'C is fun';
+console.log(c);
+const p = 'Python is cool';
+console.log(p);
+const myVar = 'Javascript is amazing';
+console.log(myVar);
