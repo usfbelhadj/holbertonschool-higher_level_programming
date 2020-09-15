0ccf8b5b8742d0c90e7fd7a0d3d1031bdcf4d3a7
usfbelhadj <usf.belhadj@gmail.com>
2020-09-15 16:50:12 +0100

    Loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+First constant, first print
+*/
+'use strict';
+
+const myArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i;
+for (i = 0; i < myArr.length; i++) {
+  for (i = 0; i < myArr.length; i++) { console.log(myArr[i]); }
+}
