b6bdb27d1dad11102ce8393df1ceb22f9ab37e4c
usfbelhadj <usf.belhadj@gmail.com>
2020-09-15 19:21:05 +0100

    Test

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ First constant, first print
 */
 'use strict';
 
-if (process.argv.length == 2 || process.argv.length == 3) {
+if (process.argv.length === 2 || process.argv.length === 3) {
   console.log('0');
 } else {
   console.log(process.argv.slice(2).sort((a, b) => b - a)[1]);
