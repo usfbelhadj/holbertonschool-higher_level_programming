b0c58910f5eedc8d1c45ceed9d562eb5e060b9c3
usfbelhadj <usf.belhadj@gmail.com>
2020-09-15 18:43:38 +0100

    Fac

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,8 +5,13 @@ First constant, first print
 'use strict';
 
 const num = process.argv[2];
-let i; let sum = 1;
-for (i = num; i >= 1; i--) {
-  sum = sum * i;
+let i;
+let sum = 1;
+if (num === 0 || num === 1) {
+  console.log(sum);
+} else {
+  for (i = num; i >= 1; i--) {
+    sum = sum * i;
+  }
+  console.log(sum);
 }
-console.log(sum);
