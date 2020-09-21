55c1b458382b48511982b8e313082ab6aff43f08
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 15:49:12 +0100

    Reverrse Arry

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const arr = [];
+  let i;
+  for (i = list.length - 1; i >= 0; i--) {
+    arr.push(list[i]);
+  }
+  return arr;
 };
