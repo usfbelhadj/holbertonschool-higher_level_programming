fbab10ecf0adaa71c3c7a1d900e24f36cbdeb0cc
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 12:04:39 +0100

    Rectangle instance

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+class Rectangle
+*/
+'use strict';
+let i, j;
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
