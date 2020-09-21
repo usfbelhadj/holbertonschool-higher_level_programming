4a8c0a14419c70fc51e5aaf72f6798095bfc6f7a
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 14:06:55 +0100

    Fix style

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,14 +6,15 @@ class Rectangle
 
 let i;
 class Rectangle {
-    constructor (w, h) {
-        if (w > 0 && h > 0) {
-          this.width = w;
-          this.height = h;
-        } else {
-          return Rectangle === {};
-        }
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return Rectangle === {};
+    }
+  }
+
   print () {
     for (i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
