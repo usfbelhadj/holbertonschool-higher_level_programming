0234160776683439aa28873d07599c0f1e4eac1f
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 14:05:32 +0100

    Fix Code

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,11 +6,14 @@ class Rectangle
 
 let i;
 class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
+          this.width = w;
+          this.height = h;
+        } else {
+          return Rectangle === {};
+        }
+      }
   print () {
     for (i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
