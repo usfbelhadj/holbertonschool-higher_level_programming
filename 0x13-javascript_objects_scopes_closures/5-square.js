01329d0444b5eb50e51e11a6adcad7bfb466b3fa
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 13:55:25 +0100

    Test

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -6,7 +6,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-      if (size > 0)
+      if (size >= 0)
       {
           super(size, size);
       }
