e72efa8280290c1c66d4d6181f3e724c4d9cd67f
usfbelhadj <usf.belhadj@gmail.com>
2020-09-21 12:26:12 +0100

    Rotate, Double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+/*
+class Rectangle
+*/
+'use strict';
+
+let i;
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let rot;
+    rot = this.width;
+    this.width = this.height;
+    this.height = rot;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
