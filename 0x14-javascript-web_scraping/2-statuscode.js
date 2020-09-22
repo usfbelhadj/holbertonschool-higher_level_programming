65e986f5be3af84f123f3930dd23552d0fee1f56
usfbelhadj <usf.belhadj@gmail.com>
2020-09-22 17:26:08 +0100

    Fix Style

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 Display the status code of a GET request
 */
 const request = require('request');
-request.get(process.argv[2], function (error, response){
-    if (error){
-        console.error('error:', error);
-    }
-    console.log('code:', response && response.statusCode);
-  });
+request.get(process.argv[2], function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  }
+  console.log('code:', response && response.statusCode);
+});
