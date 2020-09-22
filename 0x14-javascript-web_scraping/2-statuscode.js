710dfa65e49ab651a0000d52d3b88e1dfc78d65a
usfbelhadj <usf.belhadj@gmail.com>
2020-09-22 17:25:34 +0100

    SatatusCode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+Display the status code of a GET request
+*/
+const request = require('request');
+request.get(process.argv[2], function (error, response){
+    if (error){
+        console.error('error:', error);
+    }
+    console.log('code:', response && response.statusCode);
+  });
