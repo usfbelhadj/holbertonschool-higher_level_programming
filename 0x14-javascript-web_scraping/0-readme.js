5c35c2c32ad3bd504f35e971938dc411c0a58b2d
usfbelhadj <usf.belhadj@gmail.com>
2020-09-22 16:42:01 +0100

    Fix CODE

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 /*
-eads and prints the content of a file.
+Reads and prints the content of a file.
 */
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function read (err, data) {
-  if (err) throw err;
-  console.log(data);
+fs.readFile(process.argv[2], 'utf8',function(err, data){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(data);
+    }
 });
